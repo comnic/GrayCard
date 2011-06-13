a4ab940e90191f817504036c7adad73289889bfc
Minho Lee <comnic@hanmail.net>
2011-06-14 00:36:28 +0900

    노출보정카드, 색상보정카드 추가.

--- src/kr/comnic/GrayCard/GrayCardActivity.java
+++ src/kr/comnic/GrayCard/GrayCardActivity.java
@@ -2,11 +2,17 @@ package kr.comnic.GrayCard;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
-import com.google.ads.*;
+import com.google.ads.AdRequest;
+import com.google.ads.AdSize;
+import com.google.ads.AdView;
 
 public class GrayCardActivity extends Activity {
+	View viewCard1, viewCard2, viewCard3;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,39 @@ public class GrayCardActivity extends Activity {
         // Add the adView to it
         layout.addView(adView);
         // Initiate a generic request to load it with an ad
-        adView.loadAd(new AdRequest());        
+        adView.loadAd(new AdRequest());
+        
+        viewCard1 = findViewById(R.id.imgGrayCard);
+        viewCard2 = findViewById(R.id.imgWGBCard);
+        viewCard3 = findViewById(R.id.imgStopCard);
+        
+        findViewById(R.id.btnGrayCard).setOnClickListener(btnOnClickListener);
+        findViewById(R.id.btnWGBCard).setOnClickListener(btnOnClickListener);
+        findViewById(R.id.btnStopCard).setOnClickListener(btnOnClickListener);
     }
+    
+    Button.OnClickListener btnOnClickListener = new Button.OnClickListener(){
+
+		@Override
+		public void onClick(View v) {
+			viewCard1.setVisibility(View.INVISIBLE);
+			viewCard2.setVisibility(View.INVISIBLE);
+			viewCard3.setVisibility(View.INVISIBLE);
+
+			// TODO Auto-generated method stub
+			switch(v.getId()){
+			case R.id.btnGrayCard :
+				viewCard1.setVisibility(View.VISIBLE);
+				break;
+			case R.id.btnWGBCard :
+				viewCard2.setVisibility(View.VISIBLE);
+				break;
+			case R.id.btnStopCard :
+				viewCard3.setVisibility(View.VISIBLE);
+				break;
+			}
+			
+		}
+    	
+    };
 }
